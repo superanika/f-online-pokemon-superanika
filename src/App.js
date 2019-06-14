db468d8d4a71b918766a7633e8806b448ae274de
superanika <anamaria.romerogarcia@gmail.com>
2019-06-14 12:55:00 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+
+        hola!
+      </div>
+    );
+
+  }
+}
+
+export default App;
