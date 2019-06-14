78ecb621d32829716397044b3e7fba32ec8054ea
superanika <anamaria.romerogarcia@gmail.com>
2019-06-14 12:56:28 +0200

    create docs folder

---
+++ docs/precache-manifest.2ffcd782b02e5b2401a8cac0a0e7e2d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca62afd84b2a0a6b11f99c3b17b6e2cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6756c2e2ab5744004198",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6756c2e2ab5744004198",
+    "url": "./static/js/main.26521393.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
