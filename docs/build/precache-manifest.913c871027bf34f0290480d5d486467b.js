4c084c2eb4f8102dfff9f0e48c9e5159206f0d36
superanika <anamaria.romerogarcia@gmail.com>
2019-06-17 09:46:25 +0200

    create docs

---
+++ docs/build/precache-manifest.913c871027bf34f0290480d5d486467b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c5f6bfcaa399edac340648b8e8a701",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15b553fb5f3e99378d39",
+    "url": "./static/css/main.bcb18975.chunk.css"
+  },
+  {
+    "revision": "89655a28ff80f3017410",
+    "url": "./static/js/2.40dc30cc.chunk.js"
+  },
+  {
+    "revision": "15b553fb5f3e99378d39",
+    "url": "./static/js/main.ae36144b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "beb53ef3ffd934e347a430522551eff9",
+    "url": "./static/media/Pokedex.beb53ef3.png"
+  },
+  {
+    "revision": "2ae99ccb067c14f0ff9df18a67869d5c",
+    "url": "./static/media/pikachu.2ae99ccb.png"
+  }
+]);
